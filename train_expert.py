4cdd1458b600770c10e67a22899979aa6c517883
bavishk <bavishkulur@gmail.com>
2024-06-04 16:10:43 +0530

    Store expert data

--- train_expert.py
+++ train_expert.py
@@ -3,7 +3,7 @@ import seals
 import torch
 import numpy as np
 import random
-from stable_baselines3 import PPO, DDPG
+from stable_baselines3 import PPO, DDPG, SAC
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
@@ -11,23 +11,24 @@ import wandb
 from wandb.integration.sb3 import WandbCallback
 import time
 
-
 def make_env(env_name):
     def _init():
         env = gym.make(env_name)
         env = Monitor(env)
         return env
+    
     return _init
 
 algos = {
     "PPO": PPO,
-    "DDPG": DDPG
+    "DDPG": DDPG,
+    "SAC": SAC
 }
 
 def train_expert(env_name, training_algo, total_expert_timesteps):
     
     env = DummyVecEnv([make_env(env_name)])
-    # expert = PPO("MlpPolicy", env, verbose=0, tensorboard_log=f"runs/{run.id}", seed=SEED)
+    env.seed(SEED)
     expert = algos[training_algo]("MlpPolicy", env, verbose=0, tensorboard_log=f"runs/{run.id}", seed=SEED)
     
     expert.learn(
@@ -44,6 +45,26 @@ def train_expert(env_name, training_algo, total_expert_timesteps):
     print(f"Expert's mean reward: {mean_reward}")
     return expert
 
+def collect_trajectories(env, policy, n_episodes=10):
+    env = DummyVecEnv([make_env(env.unwrapped.spec.id)])
+    env.seed(SEED)
+    expert_data = []
+    for _ in range(n_episodes):
+        obs = env.reset()
+        done = False
+        while not done:
+            action, _ = policy.predict(obs)
+            next_obs, reward, done, info = env.step(action)
+            
+            x = np.concatenate((obs[0], next_obs[0])) # x = (s, s')
+            expert_data.append(x)
+            
+            obs = next_obs
+            
+            if done:
+                break
+    return expert_data
+
 
 SEED = 0
 torch.manual_seed(SEED)
@@ -51,10 +72,10 @@ torch.backends.cudnn.deterministic = True
 np.random.seed(SEED)
 random.seed(SEED)
 
-env_name = "seals/CartPole-v0"
+env_name = "Ant-v4"
 env = gym.make(env_name)
 
-training_algo = "PPO"
+training_algo = "SAC"
 
 expert_policy_path = f"experts/expert_{env_name}_{training_algo}.policy"
 
@@ -67,5 +88,8 @@ run = wandb.init(
 )
 
 
-expert = train_expert(env_name, training_algo=training_algo, total_expert_timesteps=100_000)
+expert = train_expert(env_name, training_algo=training_algo, total_expert_timesteps=1_000_000)
 expert.save(expert_policy_path)
+
+expert_data = collect_trajectories(env, expert, 50)
+np.save(f"expert_data/{env_name}_{training_algo}.npy", np.array(expert_data))
